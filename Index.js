2e5d1b1b2d67c3f9c58ba2f46761d5dc6db4d3f9
Trishapujaroy <roypuja442504@gmail.com>
2024-02-08 20:53:54 +0530

    My first commit

--- Index.js
+++ Index.js
@@ -1,71 +1,40 @@
-const calculatorDisplay = document.querySelector('h1');
-const inputBtns = document.querySelectorAll('button');
-const clearBtn = document.getElementById('clear-btn');
-let firstValue = 0;
-let operatorValue = '';
-let awaitingNextValue = false;
-function sendNumberValue(number) {
- // Replace current display value if first value is entered
- if (awaitingNextValue) {
- calculatorDisplay.textContent = number;
- awaitingNextValue = false;
- } else {
- // If current display value is 0, replace it, if not add number to display value
- const displayValue = calculatorDisplay.textContent;
- calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
- }
+const planets = document.querySelectorAll('.planet')
+const p_radii = [22,33,50,70,112,138,165,190]
+let p_radians = new Array(8).fill(0)
+const p_velocities = [1.607, 1.174,1,0.802, 0.434, 0.323, 0.228, 0.182]
+
+const moon = document.querySelector('#moon')
+const m_radius = 8
+let m_radians = 0
+const m_velocity = 10
+
+const p_orbits = document.querySelectorAll('.p-orbit')
+const m_orbit = document.querySelector('#m-orbit')
+
+p_orbits.forEach((p_orbit, index)=>{
+  p_orbit.style.height = `${p_radii[index]}vmin`
+  p_orbit.style.width = `${p_radii[index]}vmin`
+})
+
+setInterval( ()=> {
+  planets.forEach( (planet, index)=>{
+    planet.style.left = `${Math.cos(p_radians[index]) * p_radii[index]}vmin`
+    planet.style.top = `${Math.sin(p_radians[index]) * p_radii[index]}vmin`
+    p_radians[index] += p_velocities[index] * 0.02
+  })
+
+  moon.style.left = `${earthX() + (Math.cos(m_radians) * m_radius )}vmin`
+  moon.style.top = `${earthY() + (Math.sin(m_radians) * m_radius )}vmin`
+  m_radians += m_velocity * 0.02
+
+  m_orbit.style.left = `${earthX()}vmin`
+  m_orbit.style.top = `${earthY()}vmin`
+}, 1000/60)
+
+function earthX(){
+  return Number( planets[2].style.left.split('vmin')[0] )
 }
-function addDecimal() {
- // If operator pressed, don't add decimal
- if (awaitingNextValue) return;
- // If no decimal, add one
- if (!calculatorDisplay.textContent.includes('.')) {
- calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
- }
+
+function earthY(){
+  return Number( planets[2].style.top.split('vmin')[0] )
 }
-// Calculate first and second values depending on operator
-const calculate = {
- '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
- '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
- '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
- '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
- '=': (firstNumber, secondNumber) => secondNumber,
-};
-function useOperator(operator) {
- const currentValue = Number(calculatorDisplay.textContent);
- // Prevent multiple operators
- if (operatorValue && awaitingNextValue) {
- operatorValue = operator;
- return;
- }
- // Assign firstValue if no value
- if (!firstValue) {
- firstValue = currentValue;
- } else {
- const calculation = calculate[operatorValue](firstValue, currentValue);
- calculatorDisplay.textContent = calculation;
- firstValue = calculation;
- }
- // Ready for next value, store operator
- awaitingNextValue = true;
- operatorValue = operator;
-}
-// Add Event Listeners for numbers, operators, decimal
-inputBtns.forEach((inputBtn) => {
- if (inputBtn.classList.length === 0) {
- inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
- } else if (inputBtn.classList.contains('operator')) {
- inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
- } else if (inputBtn.classList.contains('decimal')) {
- inputBtn.addEventListener('click', () => addDecimal());
- }
-});
-// Reset all values, display
-function resetAll() {
- firstValue = 0;
- operatorValue = '';
- awaitingNextValue = false;
- calculatorDisplay.textContent = '0';
-}
-// Event Listener
-clearBtn.addEventListener('click', resetAll);
